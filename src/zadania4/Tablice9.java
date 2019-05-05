11b9f6c2c1185d2a38b794217990a2991e9fb5b8
Piotr <cortezzz2o2.pl>
2019-05-05 16:55:19 +0200

    Zadania Kubiak i inni

---
+++ src/zadania4/Tablice9.java
@@ -0,0 +1,41 @@
+package zadania4;
+
+public class Tablice9 {
+    public static void main(String[] args) {
+        int n = 10;
+        int i;
+        int j;
+        int[][] tabA = new int[n][n];
+        int[][] tabB = new int[n][n];
+        int[][] tabC = new int[n][n];
+
+        System.out.println("Tablica A");
+
+        for (i=0;i<tabA.length; i++){
+            for (j =0; j<tabA.length; j++){
+                tabA[i][j]=1;
+
+                    System.out.print(tabA[i][j]+" ");
+                }
+            System.out.println();
+            }
+        System.out.println("Tablica B");
+        for (i=0;i<tabB.length; i++){
+            for (j =0; j<tabB.length; j++){
+                tabB[i][j]=2;
+
+                System.out.print(tabB[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("Suma tablicy A i B");
+        for (i=0;i<tabC.length; i++){
+            for (j =0; j<tabC.length; j++){
+                tabC[i][j]=tabA[i][j]+tabB[i][j];
+
+                System.out.print(tabC[i][j]+" ");
+            }
+            System.out.println();
+        }
+        }
+    }
